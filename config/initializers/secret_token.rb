353b28f675100a24a6006953d48a3d46dc46e0fa
Robert Fruchtman <rfruchtose@gmail.com>
2013-12-01 21:01:22 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AntipatternTest::Application.config.secret_token = '18a677e8c004e9a6c8499fc85b3cca8fcd999755fae8d326e2f4ad3db8040641fe4e16f9feee12f4052c8c9a17f89a90f3b17dfbd3ed28178c83830811c635b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AntipatternTest::Application.config.session_store :cookie_store, key: '_antipattern_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AntipatternTest::Application.config.session_store :active_record_store
